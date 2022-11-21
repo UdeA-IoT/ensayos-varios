8aeae29d44cf7b320aa85d2b93beb22ae0845314
tigarto <helpu1409@gmail.com>
2022-11-21 00:16:15 -0500

    feat: update to run as a python script

--- example1/main.py
+++ example1/main.py
@@ -3,6 +3,11 @@ from flask import Flask, request, render_template, make_response, redirect
 
 app = Flask(__name__)
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
 @app.route('/')
 def index():
     
@@ -17,3 +22,7 @@ def home():
     }
     return render_template('index.html', **context)
     
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+    #app.run(debug=True)
+    #app.run()
