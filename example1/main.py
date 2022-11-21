f1ee2e773874c74b7cad610f0877f2f46ceccb7f
tigarto <helpu1409@gmail.com>
2022-11-20 22:04:22 -0500

    feat: Update to use template

--- example1/main.py
+++ example1/main.py
@@ -1,8 +1,10 @@
 
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    client_ip = request.remote_addr
+    # return 'Hello, World! to {}'.format(client_ip)
+    return render_template("hello.html", client_ip = client_ip)
